f98d57e4ebf5b73c188ef8def38821db3ba00e82
pwl <ituitu@qq.com>
2014-03-13 12:34:00 +0800

    add

---
+++ readLog/复件 refreshall.jsx
@@ -0,0 +1,9 @@
+﻿#target photoshop
+#include "C:\\Documents and Settings\\Administrator\\git\\readNews_remote\\readLog\\include_win.jsx"
+
+var fileRef = File("C:\\Documents and Settings\\Administrator\\git\\readNews_remote\\readLog\\image\\lib.psd") 
+var dest = app.open(fileRef)    
+app.activeDocument = dest
+collegeInfo("libXML.xml")
+$.write("ok")
+fileRef.close();
