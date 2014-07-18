6399a1f8fdbc5354b2b15cdcd659a64dd262577e
pwl <ituitu@qq.com>
2014-07-18 17:24:40 +0800

    update

--- readLog/refreshall.jsx
+++ readLog/refreshall.jsx
@@ -1,7 +1,1 @@
-﻿#target photoshop
-#include "/Users/pwl/git/readNews_remote/readLog/include.jsx"
-var fileRef = File("/Users/pwl/git/readNews_remote/readLog/image/lib.psd") 
-var dest = app.open(fileRef)    
-app.activeDocument = dest
-collegeInfo("libXML.xml")
-$.write("ok")
+#target photoshop#include "/Users/pwl/git/readNews_remote/readLog/include.jsx"var fileRef = File("/Users/pwl/git/readNews_remote/readLog/image/lib.psd") var dest = app.open(fileRef)    app.activeDocument = destcollegeInfo("libXML.xml")$.write("ok")
